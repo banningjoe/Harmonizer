e27cd567138cb34276febaccc6018b02ff8f743b
banningjoe <banningjoe@gmail.com>
2023-08-11 17:21:59 -0400

    Added Lower Harmonies

--- ListenAndTalk.py
+++ ListenAndTalk.py
@@ -32,6 +32,7 @@ amplitude_threshold = 0.00001  # Adjust this threshold as needed
 
 intervals = ["Unison","Octave","Perfect Fifth","Perfect Fourth","Major Sixth","Major Third","Minor Sixth","Minor Third","Major 2nd","Minor 7th","Major 7th","Minor 2nd", "Tritone"]
 interval_ratios = [1/1,2/1,3/2,4/3,5/3,5/4,8/5,6/5,9/8,16/9,15/8,16/15,7/5]
+interval_ratios_below = [1/1,1/2,2/3,3/4,3/5,4/5,5/8,5/6,8/9,9/16,8/15,15/16,5/7]
 
 # Initialize PyAudio
 p = pyaudio.PyAudio()
@@ -120,9 +121,6 @@ def animate(i):
         interval, diff = calculate_interval(sung_tone_freqs[-1],played_tone_freqs[-1])
         print(interval + ", Difference of: " + str(diff) + " hz")
 
-
-    
-
     return line_played, line_sung
 
 # Create a thread for playing the tone
@@ -147,12 +145,18 @@ def play_song_thread():
 
 def calculate_interval(sung_pitch, played_pitch):
     pitch_ratio = sung_pitch / played_pitch
-    relative_pitches = interval_ratios - pitch_ratio
-    abs_relative_pitches = [abs(diff) for diff in relative_pitches]
-    closest_interval = min(abs_relative_pitches)
-    index = abs_relative_pitches.index(closest_interval)
-
-    pitch_to_hit = played_pitch*interval_ratios[index]
+    if pitch_ratio < 1:
+        relative_pitches = interval_ratios_below - pitch_ratio
+        abs_relative_pitches = [abs(diff) for diff in relative_pitches]
+        closest_interval = min(abs_relative_pitches)
+        index = abs_relative_pitches.index(closest_interval)
+        pitch_to_hit = played_pitch*interval_ratios_below[index]
+    else:
+        relative_pitches = interval_ratios - pitch_ratio
+        abs_relative_pitches = [abs(diff) for diff in relative_pitches]
+        closest_interval = min(abs_relative_pitches)
+        index = abs_relative_pitches.index(closest_interval)
+        pitch_to_hit = played_pitch*interval_ratios[index]
 
     return intervals[index], pitch_to_hit - sung_pitch
 
